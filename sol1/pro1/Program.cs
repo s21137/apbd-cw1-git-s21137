1bdb4254731c22d5d7362daa3d78a181fd8619d7
Dawid Śliwka <s21137@pjwstk.edu.pl>
2026-03-19 09:28:36 +0100

    Dodanie metody Dodaj

--- sol1/pro1/Program.cs
+++ sol1/pro1/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine(Dodaj(1,1));
+
+
+
+int Dodaj(int liczba1, int liczba2)
+{
+    return liczba1 + liczba2;
+}
